72626231ed27ec5134049dc0a98bcae5e2a3bc92
TopcodeGR <topcodegr@gmail.com>
2025-03-07 11:43:10 +0200

    refactor

--- build.sh
+++
@@ -1,7 +0,0 @@
-docker build  --no-cache \
-  --build-arg REPOSILITE_USERNAME=$REPOSILITE_USERNAME \
-  --build-arg REPOSILITE_PASSWORD=$REPOSILITE_PASSWORD \
-  -t registry.digitalocean.com/topcode/cecloudv2-platform:dev .
-
-
-docker push registry.digitalocean.com/topcode/cecloudv2-platform:$RELEASE_BRANCH
